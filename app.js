185d8a5b9ad9ebc1c5fbdff1506027d0723924a3
TylerD239 <tylerd239@gmail.com>
2020-04-02 18:45:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(PORT, ()=>console.log(`started on port ${PORT}`))
 
 app.use('/', express.static(path.join(__dirname,'client', 'build')))
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
